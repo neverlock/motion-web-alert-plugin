2f2f081d87126a0d070d4373e87ba0956f7dfc84
chagridsada boonthus <chagridsada@chagridsada.(none)>
2011-10-26 23:07:54 +0700

    modified script sent sms

--- usr/local/bin/motion-web-alert-plugin/sms_send.sh
+++ usr/local/bin/motion-web-alert-plugin/sms_send.sh
@@ -2,18 +2,19 @@
 if [ -z $1 ]; then echo "\nEx.\n\t./sms_send.sh 'SMS_GATEWAY_USER' 'SMS_GATEWAY_PASSWORD' 'SMS_GATEWAY_URL' 'SMS_GATEWAY_POST' 'INTRUDE_SMS_MSG' 'URL_IMAGE' 'INTRUDE_SMS_PHONE'\n"; return 0; fi
 . /usr/local/bin/motion-web-alert-plugin/write_log.sh
   USERNAME=$1; PASSWORD=$2; URL_POST=$3; POST=$4; MESSAGE=$5; URL_IMAGE=$6; PHONES=$7
-  MESSAGE="$MASSAGE $URL_IMAGE"
+  MESSAGE="$MESSAGE $URL_IMAGE"
   write_log "SMS Send..."
   write_log "Massage : $MESSAGE"
   MESSAGE=`echo $MESSAGE | tr " " "+"`
+  POST=`echo "$POST" | sed -e 's/$USERNAME/'$USERNAME/g|sed -e 's/$PASSWORD/'$PASSWORD/g|sed -e 's/$MESSAGE/'$MESSAGE/g`
   COUNT_FIELD=`echo $PHONES |awk -F"," '{print NF}'`
   i=1
   write_log "Phone : $COUNT_FIELD Number"
   echo $PHONES | tr ',' "\n" | while read PhoneNO
   do
     write_log "  $i.Sending sms to $PhoneNO"
-    #result=`curl -s -d "$POST" $URL_POST`
-    #result=`curl -s -d "Username=$USERNAME&Password=$PASSWORD&Text=$MESSAGE&PhoneNumber=$PhoneNO&SMSMode=E&SName=CCTV" $URL_POST`
+    NEW_POST=`echo "$POST" | sed -e 's/$PhoneNO/'$PhoneNO/g`
+    result=`curl -s -d "$NEW_POST" $URL_POST`
     if [ -z $result  ]
     then 
       write_log "  Send Failed!"
